5f95f3c6044926d48afc65dd442f24e09cbfa4bc
snowvlrz <jmirandadasilva2@gmail.com>
2025-04-17 14:55:37 -0300

    Add files via upload

---
+++ variavel/Variavel.py
@@ -0,0 +1,30 @@
+# Criando variável
+peso = float()
+altura = float()
+nome = str()
+idade = int()
+# Imprimindo a variável
+print(peso)
+print(altura)
+print(nome)
+print(idade)
+# Imprimindo os tipos de variáveis
+print(type(nome))
+print(type(idade))
+print(type(peso))
+print(type(altura))
+# Imrpimindo o texto mais o tipo da variável
+print("o tipo da varíavel ", nome, "é ", type(nome))
+print("o tipo da varíavel ", idade, "é ", type(idade))
+print("o tipo da varíavel ", peso, "é ", type(peso))
+print("o tipo da varíavel ", altura, "é ", type(altura))
+# Criamos um hub para o peopleware
+nome=str(input("Digite seu nome:"))
+idade=int(input("Digite sua idade:"))
+peso=float(input("Digite sua peso:"))
+altura=float(input("Digite sua altura:"))
+# Imprimindo o nomes da variáveis
+print("Seu nome é =", nome)
+print("Sua idade é =", idade)
+print("Seu peso é =", peso)
+print("Sua altura é =", altura)
