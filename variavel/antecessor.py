80b83319f378011d30b3d43120aadd96ae63d1a0
snowvlrz <jmirandadasilva2@gmail.com>
2025-05-08 14:47:47 -0300

    Add files via upload

---
+++ variavel/antecessor.py
@@ -0,0 +1,7 @@
+print(f"Olá, {__name__}")
+numero = int(input("Digite o número que deseja:"))
+antecessor = numero - 1
+sucessor = numero + 1
+print("\n")
+print(f"O antecessor de {numero} é {antecessor}.")
+print(f" O sucessor de {numero} é {sucessor}.")
---
+++ variavel/medidas.py
@@ -0,0 +1,8 @@
+km = int(input("Digite um número em KM: "))
+m = km*1000
+cm = km*10000
+mm = km*100000
+print("\n")
+print("Essa quilometragem em metros é: ", m)
+print("Em centímetros é: ", cm)
+print("Em milímetros é:", mm)
